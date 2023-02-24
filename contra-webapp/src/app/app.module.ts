97d8a89e35c47616255164e66d9209e5143892d6
Ryan Zuzelski <rzuzelski@gmail.com>
2023-02-24 15:25:44 -0800

    Add fontawesome (not working yet)

--- contra-webapp/src/app/app.module.ts
+++ contra-webapp/src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ColorPickerModule } from 'ngx-color-picker'; //https://www.npmjs.com/package/ngx-color-picker
+
+// add applications specific icons. see: https://github.com/FortAwesome/angular-fontawesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    ColorPickerModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
