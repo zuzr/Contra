ed4ca8ba8ea11c494c225464fad4b802fa251100
Jazlyn Martin <132096004+JazlynMartin@users.noreply.github.com>
2023-06-16 14:06:56 -0400

    create color inputs component

---
+++ contra-webapp/src/app/color-inputs/color-inputs.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ColorInputsComponent } from './color-inputs.component';
+
+describe('ColorInputsComponent', () => {
+  let component: ColorInputsComponent;
+  let fixture: ComponentFixture<ColorInputsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ColorInputsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ColorInputsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ contra-webapp/src/app/color-inputs/color-inputs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-inputs',
+  templateUrl: './color-inputs.component.html',
+  styleUrls: ['./color-inputs.component.scss']
+})
+export class ColorInputsComponent {
+
+}
